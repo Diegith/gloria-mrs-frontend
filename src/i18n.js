bc1fe6f0cd8d635b8c98bf233f0f88b5aef93f22
Diegith <diego.albarracinacero@gmail.com>
2026-01-11 00:08:32 -0500

    chore: change the translate file

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
@@ -17,6 +18,10 @@ i18n
       escapeValue: false, // React ya protege contra XSS
     },
 
+    backend: {
+      // Ruta donde creamos las carpetas anteriormente
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
   });
 
 export default i18n;
